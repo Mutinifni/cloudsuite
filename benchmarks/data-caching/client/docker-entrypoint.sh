b4f6a320910d24e1faebcfc80968241360dc1f96
Pratyush Patel <patelp1@cs.washington.edu>
2022-08-16 02:08:29 +0000

    Replace data-caching loader / runner with client entrypoint scripts

--- benchmarks/data-caching/client/docker-entrypoint.sh
+++ benchmarks/data-caching/client/docker-entrypoint.sh
@@ -2,21 +2,40 @@
 set -e
 set -x
 
-if [ "$1" = '-rps' ]; then
-	# default configuration
-	echo "dc-server, 11211" > "/usr/src/memcached/memcached_client/servers.txt"
-	/usr/src/memcached/memcached_client/loader \
-		-a /usr/src/memcached/twitter_dataset/twitter_dataset_unscaled \
-		-o /usr/src/memcached/twitter_dataset/twitter_dataset_5x \
-		-s /usr/src/memcached/memcached_client/servers.txt \
-		-w 4 -S 5 -D 2048 -j
+if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$  ]]; then
+	# default server configuration
+	echo "$1, 11211" > "/usr/src/memcached/memcached_client/servers.txt"
 
-	/usr/src/memcached/memcached_client//loader \
-		-a /usr/src/memcached/twitter_dataset/twitter_dataset_5x \
-		-s /usr/src/memcached/memcached_client/servers.txt \
-		-g 0.8 -c 200 -w 4 -e -r "$2" -t 123 -T 120
+    if [[ "$2" = "load" ]]; then
+        # scale up dataset and warmup keys
+	    /usr/src/memcached/memcached_client/loader \
+	    	-a /usr/src/memcached/twitter_dataset/twitter_dataset_unscaled \
+	    	-o /usr/src/memcached/twitter_dataset/twitter_dataset_30x \
+	    	-s /usr/src/memcached/memcached_client/servers.txt \
+	    	-w 4 -S 30 -D 4096 -j -T 1
+        exec "bash"
+
+    elif [[ "$2" = "find_rps" ]]; then
+        # find max throughput rps
+	    /usr/src/memcached/memcached_client/loader \
+	    	-a /usr/src/memcached/twitter_dataset/twitter_dataset_30x \
+	    	-s /usr/src/memcached/memcached_client/servers.txt \
+	    	-g 0.8 -T 1 -c 200 -w 8
+        exec "bash"
+
+    elif [[ "$2" = "run" ]]; then
+        # run benchmark
+        /usr/src/memcached/memcached_client/loader \
+    		-a /usr/src/memcached/twitter_dataset/twitter_dataset_30x \
+    		-s /usr/src/memcached/memcached_client/servers.txt \
+    		-g 0.8 -T 1 -t 120 -c 200 -w 8 -e -r "$3"
+
+    else
+        echo "Command not recognized"
+    fi
 
 else
 	# custom command
 	exec "$@"
 fi
+
